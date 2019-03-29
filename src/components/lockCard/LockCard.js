703b1a70e2bcb76092e3301948090e69068b6267
Tomasz Kawik <t.kawik@o2.pl>
2019-03-29 19:37:02 +0100

    Adjust styles for locks.

--- src/components/lockCard/LockCard.js
+++ src/components/lockCard/LockCard.js
@@ -1,103 +1,87 @@
-import React from "react";
-import classNames from "classnames";
-import { withStyles } from "@material-ui/core/styles";
-import green from "@material-ui/core/colors/green";
-import red from "@material-ui/core/colors/red";
-import Card from "@material-ui/core/Card";
-import Button from "@material-ui/core/Button";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import Tooltip from "@material-ui/core/Tooltip";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import LockIcon from "@material-ui/icons/Lock";
-import LockOpenIcon from "@material-ui/icons/LockOpen";
+import React from 'react';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import red from '@material-ui/core/colors/red';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import LockIcon from '@material-ui/icons/Lock';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
 
 const styles = theme => ({
-  title: {
-    fontSize: 14
-  },
-  wrapper: {
-    margin: theme.spacing.unit,
-    position: "relative"
-  },
-  locked: {
-    backgroundColor: red[500],
-    "&:hover": {
-      backgroundColor: red[700]
-    }
-  },
-  unlocked: {
-    backgroundColor: green[500],
-    "&:hover": {
-      backgroundColor: green[700]
-    }
-  },
-  unlockProgress: {
-    color: green[500],
-    position: "absolute",
-    top: -6,
-    left: -6
-  },
-  lockProgress: {
-    color: red[500],
-    position: "absolute",
-    top: -6,
-    left: -6
-  }
+    title: {
+        fontSize: 14,
+    },
+    wrapper: {
+        margin: theme.spacing.unit,
+        position: 'relative',
+    },
+    locked: {
+        backgroundColor: red[100],
+        '&:hover': {
+            backgroundColor: red[300],
+        },
+    },
+    unlocked: {
+        backgroundColor: green[100],
+        '&:hover': {
+            backgroundColor: green[300],
+        },
+    },
+    unlockProgress: {
+        color: green[300],
+        position: 'absolute',
+        top: -6,
+        left: -6,
+    },
+    lockProgress: {
+        color: red[300],
+        position: 'absolute',
+        top: -6,
+        left: -6,
+    },
 });
 
-const LockCard = ({
-  name,
-  id,
-  onClickLock,
-  status,
-  loading,
-  classes,
-  onUpdate,
-  noPermission
-}) => {
-  const open = status === "OPENED";
-  const lockClassname = classNames({
-    [classes.locked]: !open,
-    [classes.unlocked]: open
-  });
-  return (
-    <Card>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary">
-          {name}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <div className={classes.wrapper}>
-          <Tooltip
-            title={open ? "Lock" : "Unlock"}
-            enterDelay={500}
-            leaveDelay={200}
-          >
-            <IconButton
-              aria-label={open ? "Lock" : "Unlock"}
-              className={lockClassname}
-              onClick={loading ? () => {} : () => onClickLock({ id, status })}
-            >
-              {open ? <LockOpenIcon /> : <LockIcon />}
-            </IconButton>
-          </Tooltip>
-          {loading && (
-            <CircularProgress
-              size={60}
-              className={open ? classes.lockProgress : classes.unlockProgress}
-            />
-          )}
-        </div>
-        <Button size="small" color="primary" onClick={() => onUpdate(id)}>
-          Edit
-        </Button>
-      </CardActions>
-    </Card>
-  );
+const LockCard = ({ name, id, onClickLock, status, loading, classes, onUpdate, noPermission }) => {
+    const open = status === 'OPENED';
+    const lockClassname = classNames({
+        [classes.locked]: !open,
+        [classes.unlocked]: open,
+    });
+    return (
+        <Card>
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary">
+                    {name}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <div className={classes.wrapper}>
+                    <Tooltip title={open ? 'Lock' : 'Unlock'} enterDelay={500} leaveDelay={200}>
+                        <IconButton
+                            aria-label={open ? 'Lock' : 'Unlock'}
+                            className={lockClassname}
+                            onClick={loading ? () => {} : () => onClickLock({ id, status })}
+                        >
+                            {open ? <LockOpenIcon /> : <LockIcon />}
+                        </IconButton>
+                    </Tooltip>
+                    {loading && (
+                        <CircularProgress size={60} className={open ? classes.lockProgress : classes.unlockProgress} />
+                    )}
+                </div>
+                <Button size="small" color="primary" onClick={() => onUpdate(id)}>
+                    Edit
+                </Button>
+            </CardActions>
+        </Card>
+    );
 };
 
 export default withStyles(styles)(LockCard);
--- src/pages/locks/LocksPage.js
+++ src/pages/locks/LocksPage.js
@@ -1,74 +1,57 @@
-import React, { useContext, useEffect, Fragment, useState } from "react";
-import { withStyles } from "@material-ui/core";
-import Grid from "@material-ui/core/Grid";
-import Fab from "@material-ui/core/Fab";
-import AddIcon from "@material-ui/icons/Add";
-import Tooltip from "@material-ui/core/Tooltip";
-import { LockContext } from "../../contexts/locks";
-import { LoadingPage } from "../../components/loadingPage";
-import { LockCard } from "../../components/lockCard";
-import LockModal from "./LockModal";
+import React, { useContext, useEffect, Fragment, useState } from 'react';
+import { withStyles } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Tooltip from '@material-ui/core/Tooltip';
+import { LockContext } from '../../contexts/locks';
+import { LoadingPage } from '../../components/loadingPage';
+import { LockCard } from '../../components/lockCard';
+import LockModal from './LockModal';
 const styles = theme => ({
-  fab: {
-    position: "absolute",
-    bottom: theme.spacing.unit * 2,
-    right: theme.spacing.unit * 2
-  }
+    fab: {
+        position: 'absolute',
+        bottom: theme.spacing.unit * 2,
+        right: theme.spacing.unit * 2,
+    },
 });
 
 const LocksPage = ({ classes }) => {
-  const {
-    getLocks,
-    gettingLocks,
-    locks,
-    toggleLock,
-    togglingLock
-  } = useContext(LockContext);
+    const { getLocks, gettingLocks, locks, toggleLock, togglingLock } = useContext(LockContext);
 
-  useEffect(() => {
-    getLocks();
-  }, []);
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [selectedLockId, selectLockId] = useState(null);
-  const toggleModal = lockId => {
-    console.log(lockId);
-    selectLockId(lockId);
-    setModalOpen(!isModalOpen);
-  };
-  console.log(selectedLockId, isModalOpen);
-  return gettingLocks ? (
-    <LoadingPage />
-  ) : (
-    <Fragment>
-      <Grid container spacing={16}>
-        {locks.map(lock => (
-          <Grid key={lock.id} item xs={12} sm={6} md={3}>
-            <LockCard
-              {...lock}
-              onClickLock={toggleLock}
-              onUpdate={toggleModal}
-              loading={togglingLock[lock.id]}
-            />
-          </Grid>
-        ))}
-      </Grid>
-      <LockModal
-        isOpen={isModalOpen}
-        onClose={() => toggleModal()}
-        id={selectedLockId}
-      />
-      <Tooltip title="Add lock" enterDelay={500} leaveDelay={200}>
-        <Fab
-          color="primary"
-          aria-label="Add"
-          className={classes.fab}
-          onClick={() => toggleModal()}
-        >
-          <AddIcon />
-        </Fab>
-      </Tooltip>
-    </Fragment>
-  );
+    useEffect(() => {
+        getLocks();
+    }, []);
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [selectedLockId, selectLockId] = useState(null);
+    const toggleModal = lockId => {
+        selectLockId(lockId);
+        setModalOpen(!isModalOpen);
+    };
+    return gettingLocks ? (
+        <LoadingPage />
+    ) : (
+        <Fragment>
+            <Grid container spacing={16}>
+                {locks.map(lock => (
+                    <Grid key={lock.id} item xs={12} sm={6} md={3}>
+                        <LockCard
+                            {...lock}
+                            onClickLock={toggleLock}
+                            onUpdate={toggleModal}
+                            loading={togglingLock[lock.id]}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+            <LockModal isOpen={isModalOpen} onClose={() => toggleModal()} id={selectedLockId} />
+            <Tooltip title="Add lock" enterDelay={500} leaveDelay={200}>
+                <Fab color="primary" aria-label="Add" className={classes.fab} onClick={() => toggleModal()}>
+                    <AddIcon />
+                </Fab>
+            </Tooltip>
+        </Fragment>
+    );
 };
 const LocksPageWithStyles = withStyles(styles)(LocksPage);
 export { LocksPageWithStyles as LocksPage };
